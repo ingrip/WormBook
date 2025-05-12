1324bd168b36c17bb56d0baa30fc64807da591ae
USER <USER@USER-PC>
2025-05-11 19:50:52 -0600

    Agregar archivos de proyecto.

---
+++ WormBook/Models/PedidoEspecial.cs
@@ -0,0 +1,11 @@
+﻿namespace WormBook.Models
+{
+    public class PedidoEspecial
+    {
+        public int orderNumber { get; set; }
+        public string clientName { get; set; }
+        public string phoneNumber { get; set; }
+        public DateTime? orderDate { get; set; }
+        public int stock {  get; set; }
+    }
+}
